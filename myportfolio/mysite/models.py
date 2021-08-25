58fbece5c0c5cc476c510bbcc7b3ed5ba4813503
rigole <foplacide@gmail.com>
2021-08-25 12:00:35 +0100

    building the model for the form

--- myportfolio/mysite/models.py
+++ myportfolio/mysite/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    subject = models.CharField(max_length=200)
+    message = models.TextField(max_length=2000)
